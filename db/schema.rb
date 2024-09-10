7481308e724790e9a35da58d3fc770860bed5d5c
Umair-ah <umairahmed200203.umair@gmail.com>
2024-09-10 11:09:30 +0530

    enabled pgcrypto for uuid

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_09_10_053150) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
