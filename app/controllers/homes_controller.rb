8416b45ae13ac120d45d2a39afb1d8a36dd69a5a
Umair-ah <umairahmed200203.umair@gmail.com>
2024-09-30 11:26:22 +0530

    added  in products/show and json root path

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,18 +2,25 @@ class HomesController < ApplicationController
 
   def home
     @categories = Category.take(4)
+    @products = Product.order(updated_at: :desc).limit(4)
     respond_to do |format|
       format.html
       format.json do
-        render json: @categories.as_json(
-          except: [:created_at, :updated_at],
-          methods: [:image_url]
-        )
+        render json:{
+          category: @categories.as_json(
+              except: [:created_at, :updated_at],
+              methods: [:image_url]
+            ),
+            products: @products.as_json(
+              except: [:created_at, :updated_at],
+              methods: [:images_url]
+            )
+        }
       end 
     end
 
-    @products = Product.order(updated_at: :desc).limit(4)
 
   end
   
+
 end
